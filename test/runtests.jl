6475ca815369c2a8681053bef357a2ca5fde7e34
Sam Bond-Taylor <samb-t@hotmail.co.uk>
2024-02-28 03:10:24 +0000

    Remove from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using DiffusionModels
 using Test
 
-@testset "DiffusionModels.jl" begin
-    # Write your tests here.
-end
+# @testset "DiffusionModels.jl" begin
+#     # Write your tests here.
+# end
 
-using Cthulhu
-schedule = CosineSchedule()
-t = 0.3
-@descend alpha_cumulative(schedule, t)
+# using Cthulhu
+# schedule = CosineSchedule()
+# t = 0.3
+# @descend alpha_cumulative(schedule, t)
